a75fe7885d54625ac13f7db050f70c218d97fbda
DU <sangdyjjang@naver.com>
2025-12-17 15:44:53 +0900

    Feat: 경험 섹션 제목 및 부제목 태그 변경 - h3에서 h1, h4에서 h2로 수정

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -44,18 +44,18 @@ class Experience extends Component {
               {mainTech}
             </div>
 
-            <h3
+            <h1
               className="vertical-timeline-element-title"
               style={{ textAlign: "left" }}
             >
               {work.title}
-            </h3>
-            <h4
+            </h1>
+            <h2
               className="vertical-timeline-element-subtitle"
               style={{ textAlign: "left" }}
             >
               {work.company}
-            </h4>
+            </h2>
             <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
           </VerticalTimelineElement>
         );
