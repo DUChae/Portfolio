95b1ffc749c648e46cd565d9c390a62c4c408ac6
DU <sangdyjjang@naver.com>
2025-12-17 16:16:31 +0900

    Refactor: Skills 컴포넌트에서 아이템 매핑 로직 개선 - 변수 이름 수정 및 불필요한 요소 제거

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,21 +4,44 @@ class Skills extends Component {
   render() {
     if (this.props.sharedSkills && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
+      var skills = this.props.sharedSkills.icons.map(function (item, i) {
         return (
           <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
+            <div className="text-center skills-tile">
+              {item.icon ? (
+                <div
+                  style={{
+                    fontSize: "220%",
+                    textAlign: "center",
+                    display: "inline-block",
+                    lineHeight: "1",
+                  }}
+                >
+                  <span
+                    className="iconify"
+                    data-icon={item.icon}
+                    data-inline="false"
+                    style={{ margin: "0 auto" }}
+                  ></span>
+
                   <p
                     className="text-center"
                     style={{ fontSize: "30%", marginTop: "4px" }}
                   >
-                    {skills.name}
+                    {item.name}
+                  </p>
+                </div>
+              ) : (
+                <i className={item.class} style={{ fontSize: "220%" }}>
+                  <p
+                    className="text-center"
+                    style={{ fontSize: "30%", marginTop: "4px" }}
+                  >
+                    {item.name}
                   </p>
                 </i>
-              </div>
-            </span>
+              )}
+            </div>
           </li>
         );
       });
